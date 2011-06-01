e7bc23253894a27ad51361738d96b503a15bf2d4
Fabio C. B <bnafta@gmail.com>
2011-06-01 00:42:22 -0300

    teste

---
+++ Estudo/djocomon/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+import models
+
+
+admin.site.register(models.Chamado)
+admin.site.register(models.Secao)
---
+++ Estudo/djocomon/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+from django.utils.datetime_safe import datetime
+
+
+class Secao(models.Model):
+    nome = models.CharField(max_length=255, blank=True, null=True)
+
+
+
+class Chamado(models.Model):
+    numero = models.BigIntegerField(blank=True, null=True)
+    data_hora_criacao_chamado = models.DateTimeField(blank=True, default=datetime.now, null=True)
+    atualizado_em = models.DateTimeField(blank=True, auto_now=True, default=datetime.now ,null=True)
+
+    secao = Secao()
+
+
+
+  
